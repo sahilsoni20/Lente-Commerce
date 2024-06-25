0daaf2e8a0384ec304a837912031d3b6682087f4
sahilsoni20 <ssahil3120@gmail.com>
2024-06-25 23:25:21 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# lente-commerce
+  An fully functional lens/glasses e-commerce website 👓
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './index.css'
+
+function App() {
+
+  return (
+    <>
+    working fine
+    </>
+  )
+}
+
+export default App
